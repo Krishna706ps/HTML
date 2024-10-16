a3fbc74b4fa842c07e43a32c6a3601052d29e477
Krishna706ps <11202005884@rim.edu.bt>
2024-10-16 08:33:23 +0600

    changes

--- totalmark.c
+++ totalmark.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int marks[5], total_marks = 0;
-    float percentage;
-    int i;
-
-    
-    for (i = 0; i < 5; i++) {
-        printf("Enter marks for subject %d (out of 100): ", i + 1);
-        scanf("%d", &marks[i]);
-        total_marks += marks[i];
-    }
-
-    percentage = (total_marks / 500.0) * 100;
-
+    int c, html, ccna, book_keeping, math, total;
    
-    printf("Total marks obtained: %d\n", total_marks);
+ 
+    printf("Enter marks for c: (out of 100): ",c);
+    scanf("%d", &c);
+    printf("Enter marks for html: (out of 100): ",html);
+    scanf("%d", &html);
+    printf("Enter marks for ccna: (out of 100): ",ccna);
+    scanf("%d", &ccna);
+    printf("Enter marks for book_keepimg: (out of 100): ",book_keeping);
+    scanf("%d", &book_keeping);
+    printf("Enter marks for math:(out of 100): ",math);
+    scanf("%d", &math);
+    
+
+    total = c+html+ccna+book_keeping+math;
+    printf("Total marks obtained: %d", total);
+    float percentage;
+    percentage = total/5;
+
     printf("Percentage: %.2f%%\n", percentage);
 
     return 0;
